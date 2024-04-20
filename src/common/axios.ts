27236aee20d7031fa2e82934bbeee2cc6a98dfe4
Pyo Hyeon <megatwins@kaist.ac.kr>
2024-04-20 21:14:24 +0900

    git: ignore Creact-React-App specific files from src subtree

--- src/common/axios.ts
+++ src/common/axios.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
-import qs from "qs";
+// import axios from "axios";
+// import qs from "qs";
 
-axios.defaults.paramsSerializer = params => {
-  return qs.stringify( params, { arrayFormat: 'comma' });
-}
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+// axios.defaults.paramsSerializer = params => {
+//   return qs.stringify( params, { arrayFormat: 'comma' });
+// }
+// axios.defaults.baseURL = env.REACT_APP_API_URL;
