f18e3f0f5e42cb1bc470c7dee5151a80f75ba632
Pyo Hyeon <megatwins@kaist.ac.kr>
2024-07-27 05:00:02 +0900

    feat: src update

---
+++ lighthouse.sh
@@ -0,0 +1,7 @@
+lighthouse http://localhost:3000 --quiet --output-path=lighthouse/$1/index.html
+lighthouse http://localhost:3000/login --quiet --output-path=lighthouse/$1/login.html
+lighthouse http://localhost:3000/result/hanni --quiet --output-path=lighthouse/$1/result_hanni.html
+lighthouse http://localhost:3000/chemistry/sample --quiet --output-path=lighthouse/$1/chemistry_sample.html
+lighthouse http://localhost:3000/test --quiet --output-path=lighthouse/$1/test.html
+lighthouse http://localhost:3000/result --quiet --output-path=lighthouse/$1/result.html
+lighthouse http://localhost:3000/myChemistry --quiet --output-path=lighthouse/$1/myChemistry.html
