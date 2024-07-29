a896706e952965d6c67c9d6fb31144ec5451e90a
Pyo Hyeon <megatwins@kaist.ac.kr>
2024-07-30 05:07:13 +0900

    chore: webpack 불필요한 config 삭제

--- craco.config.ts
+++ craco.config.ts
@@ -128,13 +128,13 @@ module.exports = {
             }
             webpackConfig.module.rules = webpackConfig.module.rules.concat([
                 /* Tree-shaking on Swiper.js modules. ( https://stackoverflow.com/questions/71031894/why-isnt-webpack-tree-shaking-swiperjs-modules ) */
-                {
-                    test: /\.(mjs)$/,
-                    include: [
-                        path.resolve(__dirname, 'node_modules/swiper/modules')
-                    ],
-                    sideEffects: false
-                }
+                // {
+                //     test: /\.(mjs)$/,
+                //     include: [
+                //         path.resolve(__dirname, 'node_modules/swiper/modules')
+                //     ],
+                //     sideEffects: false
+                // }
             ])
             /** 
              * https://velog.io/@eenaree/eslint-tsconfig-paths
